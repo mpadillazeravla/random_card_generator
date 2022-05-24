acc3e6bce7950f0efa609f25eddb3cb8d5d6b9f8
mpadillazeravla <mikepadilla82@gmail.com>
2022-05-24 12:23:37 +0000

    añadido boton para cambiar carta

--- src/app.js
+++ src/app.js
@@ -4,6 +4,38 @@ import "./style.css";
 
 // funcion que al cargar la ventana genera un numero random y palo (suit) random
 window.onload = () => {
+  // funcion que genera numero random, con un array , hacemos un random al indice
+  // para que elija aleatoriamente uno de esos numeros del AS al REY
+  let generateRandomNumber = () => {
+    let numbers = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let iNumbers = Math.floor(Math.random() * numbers.length);
+    return numbers[iNumbers];
+  };
+
+  // aqui similar al random de arriba, con array, hacemos un random al indice que elige palo
+  let generateRandomsimbolo = () => {
+    let suit = ["♦", "♥", "♠", "♣"];
+    let iSuit = Math.floor(Math.random() * suit.length);
+    return suit[iSuit];
+  };
+
+  // esta variable randomsimbolo es para que nos genere el mismo palo arriba y abajo
+  // ya que si llamamos 2 veces a la funcion en el window.onload, nos carga distintos palos
+  let randomSimbolo = generateRandomsimbolo();
   document.querySelector(".number").innerHTML = generateRandomNumber(); //num random
 
   // el if sirve para poner el color rojo si es corazon o rombo
@@ -21,41 +53,9 @@ window.onload = () => {
   }
 };
 
-// funcion que genera numero random, con un array , hacemos un random al indice
-// para que elija aleatoriamente uno de esos numeros del AS al REY
-let generateRandomNumber = () => {
-  let numbers = [
-    "A",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "J",
-    "Q",
-    "K"
-  ];
-  let iNumbers = Math.floor(Math.random() * numbers.length);
-  return numbers[iNumbers];
-};
-
-// aqui similar al random de arriba, con array, hacemos un random al indice que elige palo
-let generateRandomsimbolo = () => {
-  let suit = ["♦", "♥", "♠", "♣"];
-  let iSuit = Math.floor(Math.random() * suit.length);
-  return suit[iSuit];
-};
-
-// esta variable randomsimbolo es para que nos genere el mismo palo arriba y abajo
-// ya que si llamamos 2 veces a la funcion en el window.onload, nos carga distintos palos
-let randomSimbolo = generateRandomsimbolo();
-
 // mejoras pendientes:
-// 1. cambiar fondo
-// 2. poner boton que cambie carta al hacer click
+
 // 3. cambiar carta cada X (10?¿) segundos
 // 4. Permitir que el usuario cambie tamaño de carta a traves de inputs
+
+document.getElementById("botongo").addEventListener("click", window.onload);
